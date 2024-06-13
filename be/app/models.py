913d0e7b97cf3d9403887e478d0e9d91067d749e
mcumskov <mcumskov@emeal.nttdata.com>
2024-06-12 22:00:12 -0300

    Add SQLAlquemy with postgres connection. Models User, Post

---
+++ be/app/models.py
@@ -0,0 +1,34 @@
+from werkzeug.security import check_password_hash, generate_password_hash
+from . import db
+
+class User(db.Model):
+    __tablename__ = 'user'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(40), unique=True, nullable=False)
+    name = db.Column(db.String(40), nullable=False)
+    surname = db.Column(db.String(40), nullable=False)
+    avatar = db.Column(db.Text, nullable=False)
+    password = db.Column(db.String(200), nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    created = db.Column(db.DateTime, default=db.func.current_timestamp())
+    last_login = db.Column(db.DateTime, default=db.func.current_timestamp())
+
+    def set_password(self, password):
+        self.password = generate_password_hash(password, method="sha256")
+
+    def check_password(self, password):
+        return check_password_hash(self.password, password)
+
+    def __repr__(self):
+        return f"<User {self.username}>"
+
+class Post(db.Model):
+    __tablename__ = 'post'
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(120), nullable=False)
+    content = db.Column(db.Text, nullable=False)
+    created = db.Column(db.DateTime, default=db.func.current_timestamp())
+    owner = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+    def __repr__(self):
+        return f"<Post {self.title}>"
---
+++ be/config.py
@@ -0,0 +1,13 @@
+from os import environ, path
+
+from dotenv import load_dotenv
+
+basedir = path.abspath(path.dirname(__file__))
+load_dotenv(path.join(basedir, ".env"))
+
+class Config:
+    SQLALCHEMY_DATABASE_URI = environ.get("SQLALCHEMY_DATABASE_URI")
+    SQLALCHEMY_ECHO = True
+    SQLALCHEMY_TRACK_MODIFICATIONS = environ.get("SQLALCHEMY_TRACK_MODIFICATIONS")
+
+    COMPRESSOR_DEBUG = environ.get("COMPRESSOR_DEBUG")
