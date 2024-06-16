2262751a6dd576ebec79eeb6ef15ac332c4fcf33
pablogch <pablochoconi@gmail.com>
2024-06-16 19:29:45 -0300

    login form submit event handler

--- fe/js/login.js
+++ fe/js/login.js
@@ -1,6 +1,17 @@
-const loginButton = document.getElementById('login-button');
-loginButton.addEventListener('click', () => {
+const loginForm = document.getElementById('login-form');
+
+const login = (e) => {
+  e.preventDefault(); //Evita que se recargue la página al enviar el formulario
+
+  let username = e.target['email-username'].value;
+  let password = e.target['password'].value;
+
+  // TODO
+
   errorToast('Invalid username or password');
-});
+}
+
+loginForm.addEventListener('submit', login);
+
 
 
