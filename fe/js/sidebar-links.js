3a2b66d57cc845968d95d7889d0214b0c59c4a31
pablogch <pablochoconi@gmail.com>
2024-06-22 19:27:46 -0300

    sidebar icons

--- fe/js/sidebar-links.js
+++ fe/js/sidebar-links.js
@@ -4,18 +4,25 @@ const templateLinks = document.getElementById('sidebar-links');
 const sidebarLinks = [{
   text: 'Posts',
   url: '/',
+  icon: 'fa-solid fa-newspaper'
 }, {
   text: 'Profile',
-  url: '/pages/profile/' + currentUser.username
+  url: '/pages/profile/' + currentUser.username,
+  icon: 'fa-solid fa-user'
 },{
   text: 'Exit',
   id: 'logout-button',
+  icon: 'fa-solid fa-right-from-bracket'
 }];
 
 sidebarLinks.forEach((link, i) => {
   let li = document.createElement('li');
   let a = document.createElement('a');
-  li.classList.add('nav-item', 'w-100');
+  let icon = document.createElement('i');
+  icon.classList.add('fas', ...link.icon.split(' '));
+  li.appendChild(icon);
+
+  li.classList.add('nav-item', 'w-100', 'd-flex', 'flex-row', 'align-items-center', 'ps-3');
   if(i < sidebarLinks.length - 1) {
     li.classList.add('border-bottom', 'border-color');
   }
