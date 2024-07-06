09e4a973670dfa6593933556c6f30a832f511227
pablogch <pablochoconi@gmail.com>
2024-07-06 19:52:47 -0300

    Edit my profile edit button

--- fe/js/profile.js
+++ fe/js/profile.js
@@ -4,19 +4,19 @@ let createPostTypesButtons = () => {
             icon: 'fa fa-newspaper align-content-center me-2',
             text: 'Last Posts',
             id: 'last-posts',
-            clases: 'd-flex flex-row align-items-center justify-content-center w-100 pt-2 pb-2 active',
+            clases: 'd-flex flex-row align-items-center justify-content-center w-100 pt-2 pb-2 active ps-2 pe-2',
         },
         {
             icon: 'fa fa-thumbs-up align-content-center me-2',
             text: 'Likes',
             id: 'like',
-            clases: 'd-flex flex-row align-items-center justify-content-center w-100 pt-2 pb-2 border-start border-end border-color',
+            clases: 'd-flex flex-row align-items-center justify-content-center w-100 pt-2 pb-2 border-start border-end border-color ps-2 pe-2',
         },
         {
             icon: 'fa fa-thumbs-down align-content-center me-2',
             text: 'Dislikes',
             id: 'dislike',
-            clases: 'd-flex flex-row align-items-center justify-content-center w-100 pt-2 pb-2',
+            clases: 'd-flex flex-row align-items-center justify-content-center w-100 pt-2 pb-2 ps-2 pe-2',
         },
     ];
 
@@ -36,14 +36,8 @@ let createPostTypesButtons = () => {
         let icon = document.createElement('i');
         icon.className = item.icon;
 
-        let prefix = ''
-
-        if (currentUser.username == username){
-            prefix = 'My '
-        }
-
         let text = document.createElement('span');
-        text.innerHTML = prefix + item.text;
+        text.innerHTML = item.text;
 
         button.appendChild(icon);
         button.appendChild(text);
@@ -64,19 +58,18 @@ let createProfileInfo = (user) => {
 
     let currentUser = JSON.parse(sessionStorage.getItem('currentUser'));
 
-    if (currentUser.username != user.username){
         let profileInfo = document.createElement('div');
-        profileInfo.classList.add('d-flex', 'flex-row', 'align-items-center', 'justify-content-center');
+        profileInfo.classList.add('d-flex', 'flex-column', 'align-items-center', 'justify-content-center');
 
         let nameAndUsernameContent = document.createElement('div');
-        nameAndUsernameContent.classList.add('d-flex', 'flex-column', 'align-items-center', 'text-nowrap');
+        nameAndUsernameContent.classList.add('d-flex', 'flex-column', 'align-items-center', 'text-nowrap','w-100');
 
         let nameAndLastName = document.createElement('div');
-        nameAndLastName.classList.add('d-flex', 'flex-row', 'align-items-center', 'w-100');
+        nameAndLastName.classList.add('w-100', 'text-center');
         nameAndLastName.innerText = user.name + ' ' + user.surname;
 
         let username = document.createElement('div');
-        username.classList.add('d-flex', 'flex-row', 'align-items-center', 'w-100');
+        username.classList.add('w-100', 'text-center');
         username.innerText = user.username;
         username.setAttribute('id', 'profile-username');
 
@@ -92,11 +85,26 @@ let createProfileInfo = (user) => {
         }
         image.src = imageUrl;
 
-        profileInfo.appendChild(nameAndUsernameContent);
         profileInfo.appendChild(image);
+        profileInfo.appendChild(nameAndUsernameContent);
         profileContainer.appendChild(profileInfo);
         profileContainer.classList.remove('d-none');
-    }
+
+        if (currentUser.username === user.username) {
+          let editButtonContainer = document.createElement('div');
+          editButtonContainer.classList.add('d-flex', 'flex-column', 'align-items-center', 'w-100');
+          let editButton = document.createElement('a');
+          editButton.classList.add('btn', 'btn-success', 'mt-2');
+          editButtonIcon = document.createElement('i');
+          editButtonIcon.classList.add('fa', 'fa-pen', 'me-2');
+          editButton.appendChild(editButtonIcon);
+          editButton.innerHTML += 'Edit Profile';
+          editButton.addEventListener('click', () => {
+              window.location.href = '/pages/profile-edit?username=' + user.username;
+          });
+          editButtonContainer.appendChild(editButton);
+          profileContainer.appendChild(editButtonContainer);
+        }
 };
 
 let updatePosts = (filter) => {
